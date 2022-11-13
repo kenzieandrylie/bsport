82b4a249000abfd4c833f8b96fb2cef9109c76b1
Kenzie <andryliekenzie@gmail.com>
2022-11-14 00:47:14 +0700

    buat component userList

--- resources/js/Components/Additional/SearchBar.jsx
+++ resources/js/Components/Additional/SearchBar.jsx
@@ -1,5 +1,5 @@
-import { Link } from "@inertiajs/inertia-react";
 import React, {useState, useEffect} from "react";
+import UserList from "./UserList";
 
 const SearchBar = ({users}) => {
 
@@ -10,7 +10,7 @@ const SearchBar = ({users}) => {
             <div className="flex flex-col">
                 <input
                     type="text"
-                    placeholder="Type here"
+                    placeholder="Search Friend"
                     className="input input-bordered w-full max-w-xs"
                     onChange={(e) => setQuery(e.target.value)}
                     value={query}
@@ -24,19 +24,7 @@ const SearchBar = ({users}) => {
                         .map((user,i) => {
                             return(
                                 <>
-                                    <Link href='#'>
-                                        <div key={i} className="w-full">
-                                            <ul role="list" className="p-6 divide-y divide-slate-200">
-                                                    <li className="flex py-4 first:pt-0 last:pb-0">
-                                                        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="" className="h-10 w-10 rounded-full"/>
-                                                        <div className="ml-3 overflow-hidden">
-                                                            <p className="text-sm font-medium text-slate-900">{user.name + ' ' + user.last_name}</p>
-                                                            <p className="text-sm text-slate-500 tuncate">{user.kode}</p>
-                                                        </div>
-                                                    </li>
-                                            </ul>
-                                        </div>
-                                    </Link>
+                                    <UserList user={user} i={i}/>
                                 </>
                             )
                         }): null}
